fb9f7fed96149e6b63039902957dd70dc3e1bbb7
Aran Vella <azzavella@gmail.com>
2023-04-11 12:09:32 +0100

    Chat add, remove and edit functionality

--- App.js
+++ App.js
@@ -214,6 +214,83 @@ const addContact = async({ navigation }, id) => {
   });
 }
 
+const addContactToChat = async({ navigation }, id, chat_id) => {
+  console.debug("addContactToChat")
+  console.debug("ID: " + id);
+
+  const token = await AsyncStorage.getItem('x-authorization');
+
+  fetch(`http://192.168.1.245:3333/api/1.0.0/chat/${chat_id}/user/${id}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'X-Authorization': token,
+    },
+  })
+  .then(async(response) => {
+    
+    if (response.status == 200) {
+      Alert.alert('Alert', 'Contact Added to chat', 
+        {text: 'OK'});
+      getChat({ navigation }, chat_id);
+    }
+    else if (response.status == 400) {
+      Alert.alert('Alert', 'Already in chat', 
+        {text: 'OK'});
+    }
+    else {
+      throw new Error('Server response not OK - ' + response.status); 
+    }
+  })
+  .catch((error) => {
+    console.warn(error);
+  });
+}
+
+const removeContactFromChat = async({ navigation }, id, chat_id) => {
+  console.debug("removeContactFromChat")
+  console.debug("ID: " + id);
+
+  const token = await AsyncStorage.getItem('x-authorization');
+  const userId = await AsyncStorage.getItem('id');
+
+  fetch(`http://192.168.1.245:3333/api/1.0.0/chat/${chat_id}/user/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'X-Authorization': token,
+    },
+  })
+  .then(async(response) => {
+    
+    if (response.status == 200) {
+      if (userId == id)
+      {
+        Alert.alert('Alert', 'You have left the chat', 
+        {text: 'OK'});
+        getChats({ navigation })
+      }
+      else{
+      Alert.alert('Alert', 'Contact removed from chat', 
+        {text: 'OK'});
+      getChat({ navigation }, chat_id);
+    }
+    }
+    else if (response.status == 400) {
+      Alert.alert('Alert', 'Error', 
+        {text: 'OK'});
+    }
+    else {
+      throw new Error('Server response not OK - ' + response.status); 
+    }
+  })
+  .catch((error) => {
+    console.warn(error);
+  });
+}
+
 const postMessage = async({ navigation }, id, message) => {
   console.debug("postMessage")
   console.debug("ID: " + id);
@@ -580,8 +657,8 @@ const getChats = async({ navigation }) => {
 
 };
 
-const checkContacts = async() => {
-  console.debug("checkContacts")
+const fetchContacts = async() => {
+  console.debug("fetchContacts")
   const token = await AsyncStorage.getItem('x-authorization');
 
   try {
@@ -808,7 +885,7 @@ function ContactScreen({ route , navigation }) {
 
   useEffect(() => {
     const fetchData = async (userId) => {
-      const contacts =  await checkContacts();
+      const contacts =  await fetchContacts();
       const blocked =  await checkBlocked();
       setContactExists(contacts.some(user => user.user_id === userId));
       setBlockedExists(blocked.some(user => user.user_id === userId));
@@ -947,9 +1024,11 @@ function ChatScreen({ route, navigation }) {
 
   const formatDate = (timestamp) => {
     const date = new Date(timestamp);
+    const day = date.getDate();
+    const month = date.getMonth();
     const hours = date.getHours();
     const minutes = date.getMinutes().toString().padStart(2, '0');
-    return `${hours}:${minutes}`;
+    return `${day}/${month+1} ${hours}:${minutes}`;
   };
 
   // Sort messages by timestamp in ascending order
@@ -961,18 +1040,15 @@ return (
     behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
     style={currentStyles.container}
   >
-    <Text style={currentStyles.title}>{json.name}</Text>
+    <TouchableOpacity onPress={() => navigation.navigate('Chat Details', {json, id})}>
+        <Text style={currentStyles.title}>{json.name}</Text>
+      </TouchableOpacity>
     <FlatList
       style={currentStyles.list}
       data={sortedMessages}
       renderItem={({ item, index }) => (
         <View
-          style={[
-            currentStyles.messageContainer,
-            item.author === id
-              ? currentStyles.messageContainerSent
-              : currentStyles.messageContainerReceived,
-          ]}
+          style={currentStyles.messageContainer}
         >
           <Text style={currentStyles.authorName}>{item.author.first_name}</Text>
           <View style={currentStyles.message}>
@@ -998,6 +1074,103 @@ return (
   </KeyboardAvoidingView>
 );
 }
+
+function AddToChatScreen({ route , navigation }) {
+  console.debug("AddToChatScreen")
+  const { currentStyles } = useContext(ThemeContext);
+  const {json, chat_id} = route.params;
+  console.debug("contacts: " + json);
+
+  const renderContact = ({ item }) => (
+    <View style={currentStyles.contactContainer}>
+      <TouchableOpacity
+        style={currentStyles.addButton}
+        onPress={() => addContactToChat({ navigation }, item.user_id, chat_id)}
+      >
+        <Text style={currentStyles.addButtonText}>Add</Text>
+      </TouchableOpacity>
+      <Text style={currentStyles.contactText}>{item.email}</Text>
+      
+    </View>
+  );
+
+  return (
+      <View style={currentStyles.container}>
+        <View style={currentStyles.titleContainer}>
+          <Text style={currentStyles.whatsThat}>whatsThat</Text>
+          <Image style={currentStyles.logo} source={require('./assets/logo.png')} />
+        </View>
+        <View style={currentStyles.userInfo}>
+            <Text style={currentStyles.titleText}>Contacts:</Text>
+          </View>
+          <FlatList
+            data={json}
+            renderItem={renderContact}
+            keyExtractor={item => item.user_id}
+          />
+      </View>
+  );
+};
+
+function ChatDetailsScreen({ route , navigation }) {
+  console.debug("ChatDetailsScreen")
+  const { currentStyles } = useContext(ThemeContext);
+  const {json, id} = route.params;
+  console.debug("json: " + json);
+  
+  const renderMember = ({ item }) => (
+    <View style={currentStyles.contactContainer}>
+      <TouchableOpacity
+        style={currentStyles.addButton}
+        onPress={() => removeContactFromChat({ navigation }, item.user_id, id)}
+      >
+        <Text style={currentStyles.addButtonText}>Remove</Text>
+      </TouchableOpacity>
+      <Text style={currentStyles.contactText}>{item.email}</Text>
+      
+    </View>
+  );
+
+  const addToChat = async () => {
+    const contacts = await fetchContacts();
+    console.log(contacts);
+    navigation.navigate('Add To Chat', {json: contacts, chat_id: id});
+  };
+
+  return (
+      <View style={currentStyles.container}>
+        <View style={currentStyles.titleContainer}>
+          <Text style={currentStyles.whatsThat}>whatsThat</Text>
+          <Image style={currentStyles.logo} source={require('./assets/logo.png')} />
+        </View>
+        <View style={currentStyles.profileContainer}>
+          <View style={currentStyles.userInfo}>
+            <Text style={currentStyles.titleText}>Chat name:</Text>
+            <Text style={currentStyles.text}>{json.name}</Text>
+          </View>
+          <View style={currentStyles.userInfo}>
+            <Text style={currentStyles.titleText}>Creator email:</Text>
+            <Text style={currentStyles.text}>{json.creator.email}</Text>
+          </View>
+          <View style={currentStyles.userInfo}>
+            <Text style={currentStyles.titleText}>Members:</Text>
+          </View>
+          <FlatList
+            data={json.members}
+            renderItem={renderMember}
+            keyExtractor={item => item.id}
+          />
+        </View>
+        <TouchableOpacity style={currentStyles.btn} onPress={() =>  navigation.navigate('Edit Chat', {json: json, id: id})}>
+        <Text style={currentStyles.btnText}>Edit Chat Name</Text>
+      </TouchableOpacity>
+        <TouchableOpacity style={currentStyles.btn} onPress={() => addToChat()}>
+        <Text style={currentStyles.btnText}>Add Contacts to chat</Text>
+      </TouchableOpacity>
+      </View>
+  );
+};
+
 
 function ProfileScreen({ route , navigation }) {
   console.debug("ProfileScreen")
@@ -1043,6 +1216,74 @@ function ProfileScreen({ route , navigation }) {
         </Pressable>
       </View>
     </View>
+  );
+};
+
+function EditChatScreen({ route, navigation }) {
+  console.debug("EditChatScreen")
+  const { isDarkMode, currentStyles } = useContext(ThemeContext);
+  const { json, id } = route.params;
+  const [inputName, setInputName] = useState(json.name);
+
+  const editChat = async(id, name) => {
+    console.debug("editChat")
+    console.debug("ID: " + id);
+    console.debug("Name: " + name);
+
+    const token = await AsyncStorage.getItem('x-authorization');
+
+    fetch(`http://192.168.1.245:3333/api/1.0.0/chat/${id}`, {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-Authorization': token,
+      },
+  
+      body: JSON.stringify({
+        name: name,
+      }),
+    })
+    .then((response) => {
+      if (response.status == 200) {
+        Alert.alert('Alert', 'Details Changed Successfully', 
+        {text: 'OK'});
+        getChats({ navigation })
+      } else {
+        Alert.alert('Alert', 'Error', 
+        {text: 'OK'});
+        throw new Error('Server response not OK - ' + response.status);
+        
+      }
+    })
+    .catch((error) => {
+      console.warn(error);
+    });
+  }
+
+  return (
+      <View style={currentStyles.container}>
+         <View style={currentStyles.titleContainer}>
+          <Text style={currentStyles.whatsThat}>whatsThat</Text>
+          <Image style={currentStyles.logo} source={require('./assets/logo.png')} />
+        </View>
+        <View style={currentStyles.profileContainer}>
+          <View style={currentStyles.inputContainer}>
+          <TextInput
+            style={currentStyles.input}
+            placeholder={inputName}
+            placeholderTextColor={isDarkMode ? '#F5F5F5' : '#c4c4c4'}
+            value={inputName}
+            onChangeText={setInputName}
+          />
+          </View>
+        </View>
+        <View style={currentStyles.container}>
+        <Pressable style={currentStyles.btn} onPress={() => editChat(id, inputName)}>
+          <Text style={currentStyles.btnText}>Confirm</Text>
+        </Pressable>
+        </View>
+      </View>
   );
 };
 
@@ -1308,6 +1549,9 @@ function App() {
           <Stack.Screen name="Profile" component={ProfileScreen} />
           <Stack.Screen name="Edit Profile" component={EditProfileScreen} />
           <Stack.Screen name="Search Results" component={SearchResultsScreen} />
+          <Stack.Screen name="Chat Details" component={ChatDetailsScreen} />
+          <Stack.Screen name="Add To Chat" component={AddToChatScreen} />
+          <Stack.Screen name="Edit Chat" component={EditChatScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </ThemeProvider>
@@ -1515,21 +1759,36 @@ const darkStyles = StyleSheet.create({
   marginLeft: 5,
   marginTop: 2,
   },
-  messageContainerSent: {
-    flexDirection: 'column',
-    marginBottom: 10,
-    alignSelf: 'flex-end',
-  },
-  messageContainerReceived: {
-    flexDirection: 'column',
-    marginBottom: 10,
-    alignSelf: 'flex-start',
-  },
   authorName: {
     fontSize: 12,
     fontWeight: 'bold',
     marginBottom: 2,
     color: '#F5F5F5',
+    },
+    contactContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      marginVertical: 10,
+      paddingHorizontal: 20,
+      width: '100%',
+      minWidth: 350,
+    },
+    contactText: {
+      color: '#F5F5F5',
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    addButton: {
+      backgroundColor: '#4CAF50',
+      borderRadius: 5,
+      paddingVertical: 5,
+      paddingHorizontal: 10,
+    },
+    addButtonText: {
+      color: '#FFFFFF',
+      fontSize: 16,
+      fontWeight: 'bold',
     },
   });
 
@@ -1734,22 +1993,36 @@ const lightStyles = StyleSheet.create({
     marginLeft: 5,
     marginTop: 2,
   },
-  messageContainerSent: {
-    flexDirection: 'column',
-    marginBottom: 10,
-    alignSelf: 'flex-end',
-  },
-  messageContainerReceived: {
-    flexDirection: 'column',
-    marginBottom: 10,
-    alignSelf: 'flex-start',
-  },
   authorName: {
     fontSize: 12,
     fontWeight: 'bold',
     marginBottom: 2,
     color: '#1c1c1c',
-    },
-  
+  },
+  contactContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 10,
+    paddingHorizontal: 20,
+    width: '50%',
+    minWidth: 350,
+  },
+  contactText: {
+      color: '#1c1c1c',
+      fontSize: 18,
+      fontWeight: 'bold',
+  },
+  addButton: {
+      backgroundColor: '#4CAF50',
+      borderRadius: 5,
+      paddingVertical: 5,
+      paddingHorizontal: 10,
+  },
+  addButtonText: {
+      color: '#FFFFFF',
+      fontSize: 16,
+      fontWeight: 'bold',
+  },
 })
 
